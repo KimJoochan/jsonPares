634c4fd0f8a6a48f4ea391a264bc46acb75b3e18
김주찬KimJC <48271542+KimJoochan@users.noreply.github.com>
2019-11-19 22:57:34 +0900

    ㄹㄴㅇㄴㅇㄹㄴㅇ
    ㄴㅇㄹㅇ

--- josn/js/app.js
+++ josn/js/app.js
@@ -2,12 +2,21 @@
  let theArray = new Array();
  let uniqueName = new Array();
  let uniqueTheme = new Array();
-
+ //테마와 이름 select option값
  let lat_array;
  let lon_array;
  let name_array;
  let open_array;
  let state_array;
+
+ function addPoint(item) {
+     var latlng = new google.maps.LatLng(parseFloat(item['lati']), parseFloat(item['longi']));
+     let title = item['name'];
+     let open = item['opening'];
+     let state = item['state'];
+     addMarker(latlng, title, open, state);
+ }
+
 
  function reset() { //배열 리셋
      lat_array = new Array();
@@ -26,7 +35,7 @@
      state_array.push(res['state']);
  }
 
- function addMarker(location, title, open, state) {
+ function addMarker(location , title, open, state) {
      switch (state) {
          case "운영중지":
              iconSet = "not.png";
@@ -48,8 +57,9 @@
          position: location,
          map: map,
          icon: iconSet,
-         //animation: google.maps.Animation.DROP
+         animation: google.maps.Animation.DROP
      });
+     map.panTo(location);
      markers.push(marker);
      //클릭시 infowindow
      var contentString = title;
@@ -73,9 +83,10 @@
          }, 3000);
      });
  }
+ let jsonData;
  $(document).ready(function () {
      $.getJSON("./test2.json", function (data) {
-         const jsonData = data[2].data;
+         jsonData = data[2].data;
          for (var item of jsonData) {
              naArray.push(item['name'].split(' ')[0]);
              theArray.push(item['theme'].split(' ')[0]);
@@ -93,12 +104,39 @@
          for (var item of uniqueTheme) {
              $('#select2').append(`<option value="${item}">${item}</option>`)
          }
-          for (var j in lat_array) {
-              var latlng = new google.maps.LatLng(parseFloat(lat_array[j]), parseFloat(lon_array[j]));
-              let title = name_array[j];
-              let open = open_array[j];
-              let state = state_array[j];
-              addMarker(latlng, title, open, state);
-          } 
+         for (var j in lat_array) {
+             var latlng = new google.maps.LatLng(parseFloat(lat_array[j]), parseFloat(lon_array[j]));
+             let title = name_array[j];
+             let open = open_array[j];
+             let state = state_array[j];
+             addMarker(latlng, title, open, state);
+         }
+     });
+     $('#select').change(function () {
+         deleteMarkers();
+         var sele = $(this).val();
+         for (let item of jsonData) {
+             if (item['name'].indexOf(sele) != -1) {
+                 addPoint(item);
+             } else if (sele == "") {
+                 addPoint(item);
+             }
+         }
+     })
+     $('#select2').change(function () {
+         deleteMarkers();
+         var sele = $(this).val();
+         for (let item of jsonData) {
+             if (item['theme'].indexOf(sele) != -1) {
+                 addPoint(item);
+             } else if (sele == "") {
+                 addPoint(item);
+             }
+         }
+     })
+
+
+     $('#delete').click(function () {
+         deleteMarkers();
      })
  })
